d882944f06a3bc890992cdb100cec1622b2c96b5
JSDenis <32075762+JSDenis@users.noreply.github.com>
2022-06-28 16:00:27 +0300

    Update post.routes.js

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -2,8 +2,8 @@ const Router = require('express')
 const router = new Router
 const MessageController  = require('../controller/post.controller')
 
-router.post('/post', MessageController.createMessage)
-router.get('/post', MessageController.getMessagesByUser)
+router.post('/message', MessageController.createMessage)
+router.get('/message', MessageController.getMessagesByUser)
 
 
 module.exports = router
