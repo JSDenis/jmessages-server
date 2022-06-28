3feea59484652ec347a902d9d102e7584f10281d
JSDenis <32075762+JSDenis@users.noreply.github.com>
2022-06-28 16:00:51 +0300

    Update post.controller.js

--- controller/post.controller.js
+++ controller/post.controller.js
@@ -3,13 +3,13 @@ class MessageController{
 
     async createMessage(req, res){
         const {date_send, content, user_id} = req.body
-        const newMessage = await db.query(`INSERT INTO message (date_send, content, user_id) VALUES ($1, $2, $3) RETURNING *`, [date_send,content, user_id])
+        const newMessage = await db.query(`INSERT INTO messages (date_send, content, user_id) VALUES ($1, $2, $3) RETURNING *`, [date_send,content, user_id])
         res.json(newMessage.rows[0])
     }
 
     async getMessagesByUser (req, res){
         const {id} = req.query
-        const getMessages = await db.query(`SELECT date_send, content FROM message WHERE user_id=$1`, [id])
+        const getMessages = await db.query(`SELECT date_send, content FROM messages WHERE user_id=$1`, [id])
         res.json(getMessages.rows)
     }
 
